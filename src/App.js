6e59241e722376322e2c1ad44d3671ac55d04f15
writepro4 <writepro4@naver.com>
2020-06-06 15:46:17 +0900

    #2 Return Types Strings and Fragments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+        <Fragment>
+          <ReturnTypes />
+        </Fragment>
+    );
+  }
 }
 
 export default App;
